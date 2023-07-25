bff99072f00923bd32c9b218c8f324a299f3b836
anonymousPolon <rickyjuwono@gmail.com>
2023-07-25 16:23:55 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Accusantium, numquam vel? Suscipit culpa doloremque delectus nam tenetur nisi sed, eum quia odio, id temporibus earum cumque autem, ducimus aut. Sed.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
